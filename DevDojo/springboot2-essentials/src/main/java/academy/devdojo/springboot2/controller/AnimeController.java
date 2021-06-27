601b909813e68e54bbad64fbb2481e63c434f37a
David <davidroussenq@gmail.com>
2021-06-27 16:36:58 -0300

    3º aula

--- DevDojo/springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ DevDojo/springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,18 +1,30 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import ch.qos.logback.classic.pattern.ClassOfCallerConverter;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
 public class AnimeController {
+
+    private DateUtil dateUtil;
+    //localhost:8080/anime/list
     @GetMapping(path = "list")
     public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Bersek"));
     }
 
