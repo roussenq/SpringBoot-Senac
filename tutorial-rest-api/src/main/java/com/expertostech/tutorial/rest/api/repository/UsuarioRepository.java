70c45c19e0a82cbec522b241c323cf9be6f64d7b
David <davidroussenq@gmail.com>
2021-06-27 23:29:51 -0300

    Delete tutorial-rest-api directory

--- tutorial-rest-api/src/main/java/com/expertostech/tutorial/rest/api/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.expertostech.tutorial.rest.api.repository;
-
-import com.expertostech.tutorial.rest.api.model.UsuarioModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
-}
